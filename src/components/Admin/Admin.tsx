e089ef1b2eb231c82c4446dbe2969011f2ac05bd
Mirza Sumaiya <mirza01.alam@gmail.com>
2020-11-11 14:30:41 +0100

    admin is done

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+
+interface IOrder{
+    id:number,
+    createdBy: string
+} 
+
 
 export default function Admin() {
-return (
-    <div>
-        <h1>hello</h1>  
-        
-    </div>
-);
+
+    
+    const defaultOrder:IOrder[]=[];
+    const [orders, setOrders] = useState(defaultOrder);
+    useEffect(()=>{
+        axios.get('https://medieinstitutet-wie-products.azurewebsites.net/api/orders?companyId=5490')
+            .then(result=>{
+                setOrders(result.data);
+    })
+    },[]);
+
+   
+
+   let orderHtml = orders.map((order:IOrder)=>{
+        return(
+            <div>
+                <p>Order id is: {order.id} and created by: {order.createdBy}</p>
+                
+            </div>
+        );
+   });
+
+    return (
+        <>
+            {orderHtml}
+        </>
+    );
 }
